7de382273c14edffbf294cfcdb2f3c856edc7b33
Tommo999 <Tommo999@users.noreply.github.com>
2019-06-27 15:08:24 +0930

    Tutorial Stage 1
    Added the first door ans made the doors open to a random answer

--- Unity/MayanMath/Assets/_Scripts/Door.cs
+++ Unity/MayanMath/Assets/_Scripts/Door.cs
@@ -9,12 +9,16 @@ public class Door : MonoBehaviour
     public int Answer;
     bool AnswerMet;
 
+    public int MinAnswer;
+    public int MaxAnswer;
+
     public GameObject DisplayPanel;
     public Material[] NumberedMaterials = new Material[9];
 
     private void Start()
     {
         DoorAnim = GetComponent<Animator>();
+        Answer = Random.Range(MinAnswer, MaxAnswer);
         if(Answer >= 0 && Answer < NumberedMaterials.Length)
         DisplayPanel.GetComponent<Renderer>().material = NumberedMaterials[Answer];
     }

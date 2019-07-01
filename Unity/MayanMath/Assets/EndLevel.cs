2eb60a97239f453043b68f6a8fbef20fc3616fa6
Tommo999 <Tommo999@users.noreply.github.com>
2019-07-01 09:41:37 +0930

    Asset Refreshment
    Performed touch ups on different assets including doors and holders

---
+++ Unity/MayanMath/Assets/EndLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    public string LevelName;
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        if(Other.tag == "Player")
+        {
+            LoadLevel();    
+        }
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+}

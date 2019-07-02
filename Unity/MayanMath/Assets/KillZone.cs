39679ca8948f69056de8b20d19d0d0054e882053
Tommo999 <Tommo999@users.noreply.github.com>
2019-07-02 10:51:46 +0930

    BIG UPDATE
    World 1 Update: Need roof

---
+++ Unity/MayanMath/Assets/KillZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    public Transform Respawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Detected: "+ other.name);
+        if(other.tag == "Player")
+        {
+            Debug.Log("Respawn");
+            other.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+            other.transform.position = Respawn.position;
+            other.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+        }
+    }
+}
